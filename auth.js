8d87ddcf52447cc6c5d76798e10c0c61b1ffcc22
saulo_santos <saulo.santos@castgroup.com.br>
2026-04-01 09:31:54 -0300

    incluir usuario

--- auth.js
+++ auth.js
@@ -27,7 +27,7 @@
     'saulomed@gmail.com':       'full',
     'lorenatablada@gmail.com':       'full',
     'devcansadosaulo@gmail.com':     'readonly',
-    'wifesa@gmail.com': 'readonly'
+    'wifesa@gmail.com': 'readonly' 
     // Adicione mais emails conforme necessário
   };
 
